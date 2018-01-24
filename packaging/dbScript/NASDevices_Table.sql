b7e92f3d76365164b7785309a7d0679f91005d91
xiliangMa <maxl@cloud-times.com>
2018-01-24 01:46:47 -0800

    db: remove tab sql files
    Signed-off-by: xiliangMa <maxl@cloud-times.com>

--- packaging/dbScript/NASDevices_Table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `NASDevices` (
-  `NasId` char(255) NOT NULL,
-  `PPDeviceID` int NOT NULL,
-  PRIMARY KEY (`NasId`)
-);
--- packaging/dbScript/PPDevices_Table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `PPDevices` (
-  `PPDeviceID` int NOT NULL,
-  `IsUsed` BOOLEAN,
-  PRIMARY KEY (`PPDeviceID`)
-);
